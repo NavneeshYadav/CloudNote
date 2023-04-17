feca7c4f71ff281b917ef32d3b10bd5eefc659c7
Navneesh Yadav <yadavnavneesh999@gmail.com>
2023-04-17 14:13:23 +0530

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 // import noteContext from "../context/notes/noteContext";
 const About = () => {
   // const a = useContext(noteContext);
-  return <div className="container">This is About Page</div>;
+  return <div className="container">It's a note app with signup and signin authenication using react js and node js</div>;
 };
 
 export default About;
